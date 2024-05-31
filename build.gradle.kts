1773517bc1dfaeefbbbad605cb52152b075c2976
Nageswara Rao <nag.samayam@gmail.com>
2024-05-31 10:20:06 +0530

    Integrate faker package to generete data for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("net.datafaker:datafaker:2.2.2")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
--- src/main/java/com/sss/student/StudentApplication.java
+++ src/main/java/com/sss/student/StudentApplication.java
@@ -1,9 +1,11 @@
 package com.sss.student;
 
+import com.sss.student.school.School;
 import com.sss.student.student.Student;
 import com.sss.student.student.StudentRepository;
 import com.sss.student.student.StudentSpecifications;
 import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,10 +28,33 @@ public class StudentApplication {
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			System.out.println("------ Hello world----");
-//			int updatedRecords = studentRepository.updateStudentBirthDate(LocalDate.now(), 1L);
-//			System.out.println("Updated records: " + updatedRecords);
-//			studentRepository.findByEmailNamedQuery("nag.samayam@gmail.com")
-//					.forEach(student -> System.out.println(student.getUuid()));
+			//			int updatedRecords = studentRepository.updateStudentBirthDate(LocalDate.now(), 1L);
+			//			System.out.println("Updated records: " + updatedRecords);
+			//			studentRepository.findByEmailNamedQuery("nag.samayam@gmail.com")
+			//					.forEach(student -> System.out.println(student.getUuid()));
+
+			// Generate fake data
+			for(int i = 1; i < 11; i++) {
+				Faker faker = new Faker();
+				String firstName = faker.name().firstName();
+				String lastName = faker.name().lastName();
+				String email = faker.internet().safeEmailAddress();
+				LocalDate birthDate = LocalDate.now();
+
+				School school = new School();
+				school.setId(1L);
+
+				Student student = Student
+						.builder()
+						.firstName(firstName)
+						.lastName(lastName)
+						.email(email)
+						.birthDate(birthDate)
+						.school(school)
+						.build();
+
+				studentRepository.save(student);
+			}
 
 			Specification<Student> specification = Specification
 					.where(StudentSpecifications.hasEmail("test@example.com"))
